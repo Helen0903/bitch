5a9be56c2a34517e7672587e24c0142fc9b39f60
Helen <helen0903brown@gmail.com>
2019-02-20 12:17:05 +0000

    Fixed unecessary import
    -

--- bitch.java
+++ bitch.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class bitch {
